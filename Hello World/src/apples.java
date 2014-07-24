fb8c3155bd50bfc3f171865f10b4173900a39c7d
Conor Keilty <conork@uebbuilders.com>
2014-07-24 14:10:47 -0700

    using multiple condition if statements && and ||

--- Hello World/src/apples.java
+++ Hello World/src/apples.java
@@ -2,26 +2,41 @@ import java.util.Scanner;
 
 class apples{
 	public static void main(String args[]){
-		double test = 6;
-		if (test>=6){
-			System.out.println("True");
+		Scanner input = new Scanner(System.in);
+		double boy, girl;
+		
+		System.out.print("Girl's age: ");
+		girl = input.nextDouble();
+		
+		System.out.print("Boys's age: ");
+		boy = input.nextDouble();
+		
+		if (girl>=18){
+			System.out.println("Girl may enter");
 		}
 		else{
-			System.out.println("False");
+			System.out.println("Girl is too young");
 		}
-		if (test==6){
-			System.out.println("True");
-		}
-		else{
-			System.out.println("False");
-		}
-		if (test<6){
-			System.out.println("True");
+		
+		if (boy>=18){
+			System.out.println("Boy may enter");
 		}
 		else{
-			System.out.println("False so adding 6");
-			test+=6;
-			System.out.println(test);
+			System.out.println("Boy is too young");
+		}
+		
+		if (boy>=18 && girl>=18){
+			System.out.println("Both boy and girl may enter");
+		}
+		else{
+			System.out.println("One or both are too young");
+		}
+		
+		if (boy>=18 || girl>=18){
+			System.out.println("One or both are old enough to enter");
+		}
+		else{
+			System.out.println("Both are too young");
 		}
 	}
 }
