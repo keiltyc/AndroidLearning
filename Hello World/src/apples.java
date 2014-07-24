8ab0d754301322e8830af0350b5c1e9166ec77b1
Conor Keilty <conork@uebbuilders.com>
2014-07-24 10:53:50 -0700

    Initial commit of apples

---
+++ Hello World/src/apples.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class apples{
+	public static void main(String args[]){
+		System.out.println("Plese input data");
+		
+		Scanner cheese = new Scanner(System.in);
+	
+		System.out.println(cheese.nextLine());
+	}
+}
