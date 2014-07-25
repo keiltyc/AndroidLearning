052d34eb82218d38d6a90198529d7929f86be21d
Conor Keilty <conork@uebbuilders.com>
2014-07-24 20:01:30 -0700

    Bucky #14 Calling an outside class

--- Hello World/src/apples.java
+++ Hello World/src/apples.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 class apples{
 	public static void main(String args[]){
-		int counter = 1;
-		while (counter<=10){
-			System.out.print("Counter is currently at: ");
-			System.out.println(counter);
-			++counter;
-		}
+		tuna tunaObject = new tuna();
+		tunaObject.simpleMessage();
 	}
 }
---
+++ Hello World/src/tuna.java
@@ -0,0 +1,6 @@
+
+public class tuna {
+	public void simpleMessage(){
+		System.out.println("Yo what up this is from the tuna class!");
+	}
+}
