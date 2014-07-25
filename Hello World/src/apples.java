d79b5e26b46f4d904e1d447c9dab3e4c27addfdc
Conor Keilty <conork@uebbuilders.com>
2014-07-24 20:04:48 -0700

    Quick tip on syso in Eclipse

--- Hello World/src/apples.java
+++ Hello World/src/apples.java
@@ -4,5 +4,6 @@ class apples{
 	public static void main(String args[]){
 		tuna tunaObject = new tuna();
 		tunaObject.simpleMessage();
+		System.out.println("in Eclipse just type syso and ctrl+space to get printline syntax.");
 	}
 }
