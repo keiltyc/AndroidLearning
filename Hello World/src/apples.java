6575177d9ace44462cd410ec749029d54a3aed6a
Conor Keilty <conork@uebbuilders.com>
2014-07-24 19:52:03 -0700

    While looping

--- Hello World/src/apples.java
+++ Hello World/src/apples.java
@@ -2,27 +2,11 @@ import java.util.Scanner;
 
 class apples{
 	public static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-		int age;
-		
-		System.out.print("Age: ");
-		age = input.nextInt();
-		
-		switch (age){
-		case 1:
-			System.out.println("age 1 you can crawl");
-			break;
-			
-		case 2:
-			System.out.println("age 2 you can talk");
-			break;
-		
-		case 3:
-			System.out.println("age 2 you can walk");
-			break;
-			
-		default:
-			System.out.println("not age 1 to 3");
+		int counter = 1;
+		while (counter<=10){
+			System.out.print("Counter is currently at: ");
+			System.out.println(counter);
+			++counter;
 		}
 	}
 }
