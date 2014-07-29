d48623d13561502e04c38597bb4a2c56dfb73d1b
Conor Keilty <conork@uebbuilders.com>
2014-07-29 15:23:35 -0700

    Else if lesson #19 bucky

--- Hello World/src/apples.java
+++ Hello World/src/apples.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 class apples{
 	public static void main(String args[]){
 		Scanner input = new Scanner(System.in);
-		int age=49;
+		int age=35;
 		
-		if (age<50){
-			System.out.println("You're still young");
+		if (age>=60){
+			System.out.println("You are over 60, a senior");
+		}
+		else if (age>=50){
+			System.out.println("You are between 50 and 60, dude.");
+		}
+		else if (age>=40){
+			System.out.println("You are between 40 and 50, bucko.");
 		}
 		else{
-			System.out.println("You're old");
-			if (age>75){
-				System.out.println("You're super old");
-			}
-			else{
-				System.out.println("You're not that old");
-			}
+			System.out.println("You are under 40, woot");
 		}
 	}
 }
